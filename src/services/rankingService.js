7a50065d1bd0bf720518b82aa234068c8bd5c912
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-05-20 13:01:26 +0300

    removed debug prints and tested rankingreducers initial state

--- src/services/rankingService.js
+++ src/services/rankingService.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 const baseUrl = "/api/ranking"
 
 const createRanking = async (credentials) => {
-  console.log('rankigService create', credentials)
   const url = baseUrl + "/new"
   const response =  await axios.post(url, credentials, getConfigObject())
   return response.data
@@ -11,7 +10,6 @@ const createRanking = async (credentials) => {
 let token = ''
 
 const getConfigObject = () => {
-    console.log('TOKEN ', token)
     return {
         headers: { 'Authorization': token }
       }
@@ -20,7 +18,6 @@ const getConfigObject = () => {
 export const tokenChanger = store => next => action => {
     next(action)
     const state = store.getState()
-    console.log("state",state)
     const newToken = state.login.token
     token = `bearer ${newToken}`
   }
---
+++ src/tests/rankingReducer.test.js
@@ -0,0 +1,13 @@
+import deepFreeze from 'deep-freeze'
+import rankingReducer from '../reducers/rankingReducer'
+
+describe('rankingReducer ', () => {
+    const initialState = []
+    it(' should return a proper initial state when called with undefined state', () => {
+        const action = {
+          type: 'DO_NOTHING'
+        }
+        const newState = rankingReducer(undefined, action)
+        expect(newState).toEqual(initialState)
+      })
+})
