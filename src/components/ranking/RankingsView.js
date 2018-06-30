16f115550126eefe30a63a5d58481ad6710481f4
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-06-30 14:21:43 +0300

    create component to view all rankings

---
+++ src/components/ranking/RankingsView.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class RankingsView extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Here are all rankings that are stored to database.</h1>
+            </div>
+        )
+    }
+}
+
+export default RankingsView
