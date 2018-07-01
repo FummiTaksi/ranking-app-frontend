a484c1ae96e0a3a64f9ce348d2d01f36ec7a8181
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-07-01 21:55:05 +0300

    implement view component for signle ranking

---
+++ src/components/ranking/RankingView.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { getRanking, deleteRanking } from '../../reducers/rankingReducer'
+
+class RankingView extends React.Component {
+
+    componentDidMount() {
+        const rankingId = this.props.location.match.params.rankingId;
+        console.log('rankingId', rankingId);
+        this.props.getRanking(this.props.location.match.params.rankingId)
+    }
+
+    orderPositions()  {
+        const copyList =  this.props.ranking.positions.slice()
+        return copyList.sort((a,b) => {
+            return a.position - b.position
+        })
+    }
+
+    renderPositionCell(positionObject) {
+        console.log('positionObject', positionObject)
+        return (
+        <tr key = {positionObject._id}>
+            <td>{positionObject.position}</td>
+            <td>{positionObject.playerName}</td> 
+            <td>{positionObject.clubName }</td>
+            <td>{positionObject.rating}</td>
+        </tr>
+        )
+    }
+    render() {
+        if (!this.props.ranking) {
+            return <p>No ranking with this id!</p>
+        }
+        console.log('this.props.ranking', this.props.ranking)
+        if (!this.props.ranking.positions) {
+            return <p>Loading ranking...</p>
+        }
+        const orderedPositions = this.orderPositions()
+
+        return (
+            <div>
+                <h3>{this.props.ranking.competitionName}</h3>
+                <table>
+                    <tbody>
+                    <tr>
+                        <th>Position</th>
+                        <th>Player</th>
+                        <th>Club</th> 
+                        <th>Rating</th>
+                    </tr>
+                    {orderedPositions.map((position) => {
+                        return this.renderPositionCell(position)
+                    })}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
+const mapDispatchToProps = {
+    getRanking,
+    deleteRanking
+}
+
+const mapStateToProps = (state) => {
+    return {
+        ranking: state.ranking,
+        credentials: state.login
+    }
+}
+
+const connectedRankingView = connect(mapStateToProps, mapDispatchToProps)(RankingView)
+
+export default connectedRankingView
