18b84bc08875f16f470f81a9df7221bbdee4d321
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-07-01 20:56:12 +0300

    implement delete and fix createranking

--- src/reducers/rankingReducer.js
+++ src/reducers/rankingReducer.js
@@ -5,11 +5,15 @@ const initialState = []
 
 const reducer = (store = initialState, action) => {
     if (action.type === 'CREATE_RANKING') {
-        const newList = [...store, action.content]
+        const newList = [...store, action.content.ranking]
         return newList
     }
     if (action.type === 'SET_RANKINGS') {
       return action.content.rankings;
+    }
+    if (action.type === 'DELETE_RANKING') {
+      const notDeleted = store.filter(b => b._id !== action.content.deletedRanking._id)
+      return notDeleted
     }
     return store 
 }
@@ -29,6 +33,23 @@ export const createRanking = (content) => {
          dispatchNotification(dispatch, 'Error at creating ranking.')
         }
       }
+}
+
+export const deleteRanking = (rankingId) => {
+  return async (dispatch) => {
+    try {
+      const response = await rankingService.deleteRanking(rankingId)
+      const message =  'Ranking ' + response.deletedRanking.competitionName + ' was deleted succsefully!'
+      dispatch({
+        type: 'DELETE_RANKING',
+        content: response
+      })
+      dispatchNotification(dispatch,message)  
+    }
+    catch(error) {
+     dispatchNotification(dispatch, 'Error while deleting ranking.')
+    }
+  }
 }
 
 export const getRankings = () => {
