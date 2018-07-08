cd799783545c67076a3c3261aaa41992aeb0cb40
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-07-08 18:03:08 +0300

    update test

--- src/tests/rankingReducer.test.js
+++ src/tests/rankingReducer.test.js
@@ -2,7 +2,7 @@ import deepFreeze from 'deep-freeze'
 import rankingReducer from '../reducers/rankingReducer'
 
 describe('rankingReducer ', () => {
-    const initialState = {allRankings: [], selectedRanking: {}}
+    const initialState = {allRankings: [], selectedRanking: {}, loading: false}
     it(' should return a proper initial state when called with undefined state', () => {
         const action = {
           type: 'DO_NOTHING'
