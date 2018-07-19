a39071cbba036fd4006368d305c6c1f7a093fcee
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-07-19 21:23:03 +0300

    start fixing RankingView lint errors

--- src/components/ranking/RankingView.js
+++ src/components/ranking/RankingView.js
@@ -1,114 +1,134 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { getRanking, deleteRanking } from '../../reducers/rankingReducer'
-import PositionList from './PositionList'
-import { Button, Icon } from 'semantic-ui-react'
+import React from 'react';
+import { connect } from 'react-redux';
+import { Button, Icon } from 'semantic-ui-react';
+import { getRanking, deleteRanking } from '../../reducers/rankingReducer';
+import PositionList from './PositionList';
 
 
 class RankingView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedIndex: 0,
+    };
+    this.toggleLeft = this.toggleLeft.bind(this);
+    this.toggleRight = this.toggleRight.bind(this);
+  }
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            selectedIndex: 0
-        }
-        this.toggleLeft = this.toggleLeft.bind(this)
-        this.toggleRight = this.toggleRight.bind(this)
-    }
+  componentDidMount() {
+    const rankingId = this.props.location.match.params.rankingId;
+    this.props.getRanking(rankingId);
+  }
 
-    componentDidMount() {
-        const rankingId = this.props.location.match.params.rankingId;
-        this.props.getRanking(rankingId)
-    }
+  orderPositions(selectedRanking) {
+    const copyList = selectedRanking.positions.slice();
+    return copyList.sort((a, b) => a.position - b.position);
+  }
 
-    orderPositions(selectedRanking)  {
-        const copyList = selectedRanking.positions.slice()
-        return copyList.sort((a,b) => {
-            return a.position - b.position
-        })
-    }
+  toggleLeft() {
+    this.setState({
+      selectedIndex: this.state.selectedIndex - 1,
+    });
+  }
 
-    toggleLeft() {
-        this.setState({
-            selectedIndex: this.state.selectedIndex - 1
-        })
-    }
+  toggleRight() {
+    this.setState({
+      selectedIndex: this.state.selectedIndex + 1,
+    });
+  }
 
-    toggleRight() {
-        this.setState({
-            selectedIndex: this.state.selectedIndex + 1
-        })
-    }
+  orderPositionGroups(orderedPositions) {
+    let i = 0;
+    return orderedPositions.reduce((currentList, currentPosition) => {
+      if (currentList[i] && currentList[i].length >= 100) {
+        i++;
+      }
+      if (!currentList[i]) {
+        currentList[i] = [];
+      }
+      currentList[i].push(currentPosition);
+      return currentList;
+    }, []);
+  }
 
-    orderPositionGroups(orderedPositions) {
-        let i = 0
-        return orderedPositions.reduce((currentList, currentPosition) => {
-            if (currentList[i] && currentList[i].length >= 100) {
-                i++;
-            }
-            if (!currentList[i]) {
-                currentList[i] = []
-            }
-            currentList[i].push(currentPosition)
-            return currentList
-        },[])
-    }
+  renderNavigationButtons(listLength) {
+    return (
+      <div id="navigationButtons">
+        <Button
+          onClick={this.toggleLeft}
+          disabled={this.state.selectedIndex === 0}
+        >
+          <Icon name="angle double left" />
+        </Button>
+        <Button
+          onClick={this.toggleRight}
+          disabled={this.state.selectedIndex === listLength - 1}
+        >
+          <Icon name="angle double right" />
+        </Button>
+      </div>
+    );
+  }
 
-    renderNavigationButtons(listLength) {
-        return (
-            <div id= "navigationButtons">
-              <Button onClick={this.toggleLeft} 
-                disabled= {this.state.selectedIndex === 0}>
-                <Icon name= "angle double left" />
-              </Button>
-              <Button onClick={this.toggleRight} 
-                disabled= {this.state.selectedIndex === listLength - 1}>
-               <Icon name= "angle double right" />
-              </Button>
-         </div>
-        )
-    }
-    render() {
-        if (this.props.ranking.loading) {
-            return (
-            <div className="ui segment">
-                <div className="ui active inverted dimmer">
-                    <div className="ui textloader">Loading ranking from database</div>
-                </div>
+  render() {
+    if (this.props.ranking.loading) {
+      return (
+        <div className="ui segment">
+          <div className="ui active inverted dimmer">
+            <div className="ui textloader">
+Loading ranking from database
             </div>
-            )
-        }
-        const selectedRanking = this.props.ranking.selectedRanking
-        if (!selectedRanking.positions) {
-            return <p>No ranking with this id!</p>
-        }
-        const orderedPositions = this.orderPositions(selectedRanking)
-        const orderedPositionGroups = this.orderPositionGroups(orderedPositions);
-        return (
-            <div>
-                <h3>{selectedRanking.competitionName}, players {orderedPositions.length}</h3>
-                <h4>Showing page {this.state.selectedIndex + 1 } / {orderedPositionGroups.length} </h4>
-                {this.renderNavigationButtons(orderedPositionGroups.length)}
-                <PositionList positions= {orderedPositionGroups[this.state.selectedIndex]}/>
-                {this.renderNavigationButtons(orderedPositionGroups.length)}
-            </div>
-            
-        )
+          </div>
+        </div>
+      );
     }
+    const selectedRanking = this.props.ranking.selectedRanking;
+    if (!selectedRanking.positions) {
+      return (
+        <p>
+No ranking with this id!
+        </p>
+      );
+    }
+    const orderedPositions = this.orderPositions(selectedRanking);
+    const orderedPositionGroups = this.orderPositionGroups(orderedPositions);
+    return (
+      <div>
+        <h3>
+          {selectedRanking.competitionName}
+, players
+          {' '}
+          {orderedPositions.length}
+        </h3>
+        <h4>
+Showing page
+          {' '}
+          {this.state.selectedIndex + 1 }
+          {' '}
+/
+          {' '}
+          {orderedPositionGroups.length}
+          {' '}
+        </h4>
+        {this.renderNavigationButtons(orderedPositionGroups.length)}
+        <PositionList positions={orderedPositionGroups[this.state.selectedIndex]} />
+        {this.renderNavigationButtons(orderedPositionGroups.length)}
+      </div>
+
+    );
+  }
 }
 
 const mapDispatchToProps = {
-    getRanking,
-    deleteRanking
-}
+  getRanking,
+  deleteRanking,
+};
 
-const mapStateToProps = (state) => {
-    return {
-        ranking: state.ranking,
-        credentials: state.login
-    }
-}
+const mapStateToProps = state => ({
+  ranking: state.ranking,
+  credentials: state.login,
+});
 
-const connectedRankingView = connect(mapStateToProps, mapDispatchToProps)(RankingView)
+const connectedRankingView = connect(mapStateToProps, mapDispatchToProps)(RankingView);
 
-export default connectedRankingView
+export default connectedRankingView;
