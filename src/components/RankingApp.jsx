c444f3601369a6dfcb885fbb48787f7e0cd141b0
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-07-21 20:41:11 +0300

    remove unused Redirect

--- src/components/RankingApp.jsx
+++ src/components/RankingApp.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  HashRouter as Router, Route, Link, Redirect,
+  HashRouter as Router, Route, Link,
 } from 'react-router-dom';
 import { connect } from 'react-redux';
 import {
